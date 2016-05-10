9838f7a95f506bef18463fe3b08fc49adad00a7a
phax <philip@helger.com>
2016-05-10 15:06:37 +0200

    Added profile

--- src/main/java/com/helger/meta/tools/buildsystem/MainCheckProjectRequiredFiles.java
+++ src/main/java/com/helger/meta/tools/buildsystem/MainCheckProjectRequiredFiles.java
@@ -115,7 +115,19 @@ public final class MainCheckProjectRequiredFiles extends AbstractProjectMain
       if (_checkFileExisting (aProject, ".travis.yml").isSuccess ())
       {
         _checkFileNotExisting (aProject, "mvn-settings-add-snapshot.py");
-        _checkFileContains (aProject, ".travis.yml", "mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V -U");
+        if (aProject.getProjectType ().hasJavaCode () && aProject.getMinimumJDKVersion ().isAtLeast8 ())
+        {
+          // jdeps only with JDK8+
+          _checkFileContains (aProject,
+                              ".travis.yml",
+                              "mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V -U -P jdeps");
+        }
+        else
+        {
+          _checkFileContains (aProject,
+                              ".travis.yml",
+                              "mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V -U");
+        }
 
         // No SNAPSHOT deployment for applications
         if (aProject.getProjectType () != EProjectType.JAVA_APPLICATION &&
