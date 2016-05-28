796672f7b4835d8b510adc4ff8d0b64d7faf910d
phax <philip@helger.com>
2016-05-28 12:43:29 +0200

    Added

--- src/main/java/com/helger/meta/tools/cmdline/MainCreateBatchFiles.java
+++ src/main/java/com/helger/meta/tools/cmdline/MainCreateBatchFiles.java
@@ -102,6 +102,8 @@ public final class MainCreateBatchFiles extends AbstractProjectMain
     _createMvnBatchFile ("clean install", "mvn_clean_install.cmd");
     _createMvnBatchFile ("clean install -DskipTests=true", "mvn_clean_install_skip_tests.cmd");
     _createMvnBatchFile ("clean install sonar:sonar", "mvn_clean_install_sonar.cmd");
+    _createMvnBatchFile ("clean install forbiddenapis:check forbiddenapis:testCheck",
+                         "mvn_clean_install_forbiddenapis.cmd");
     _createBatchFile ("call mvn javadoc:javadoc %* > ../javadoc-results.txt 2>&1", "mvn_javadoc.cmd", true);
     _createBatchFile ("git pull", "git_pull.cmd", true);
     _createBatchFile ("git gc", "git_gc.cmd", true);
