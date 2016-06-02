7a99f7d90ea5e88e081006f5fa3178f6824f5479
phax <philip@helger.com>
2016-06-02 10:33:03 +0200

    Added new tool

---
+++ src/main/java/com/helger/meta/tools/buildsystem/MainCreateExternalDependencyPOM.java
@@ -0,0 +1,80 @@
+package com.helger.meta.tools.buildsystem;
+
+import java.io.File;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Predicate;
+
+import com.helger.commons.datetime.PDTFactory;
+import com.helger.commons.microdom.IMicroDocument;
+import com.helger.commons.microdom.IMicroElement;
+import com.helger.commons.microdom.MicroDocument;
+import com.helger.commons.microdom.serialize.MicroWriter;
+import com.helger.meta.AbstractProjectMain;
+import com.helger.meta.project.EExternalDependency;
+
+/**
+ * Create a POM that contains all external dependencies defined in
+ * {@link EExternalDependency} so that default maven checks can be performed.
+ * 
+ * @author Philip Helger
+ */
+public final class MainCreateExternalDependencyPOM extends AbstractProjectMain
+{
+  private static final String NS = "http://maven.apache.org/POM/4.0.0";
+
+  public static void main (final String [] args)
+  {
+    final IMicroDocument aDoc = new MicroDocument ();
+    final IMicroElement eProject = aDoc.appendElement (NS, "project");
+    eProject.appendElement (NS, "modelVersion").appendText ("4.0.0");
+    eProject.appendElement (NS, "groupId").appendText ("com.helger");
+    eProject.appendElement (NS, "artifactId").appendText ("external-dependencies");
+    eProject.appendElement (NS, "version")
+            .appendText ("1.0.0-" + DateTimeFormatter.BASIC_ISO_DATE.format (PDTFactory.getCurrentLocalDateTime ()));
+
+    final Predicate <EExternalDependency> aFilter = (e) -> e != EExternalDependency.M2E &&
+                                                           e != EExternalDependency.JDK &&
+                                                           e != EExternalDependency.FINDBUGS_ANNOTATIONS_2 &&
+                                                           e != EExternalDependency.LOG4J2_23_CORE &&
+                                                           e != EExternalDependency.LOG4J2_23_SLF4J &&
+                                                           e != EExternalDependency.LOG4J2_23_WEB &&
+                                                           e != EExternalDependency.SERVLET_API_301 &&
+                                                           e != EExternalDependency.JETTY_92_ANNOTATIONS &&
+                                                           e != EExternalDependency.JETTY_92_JSP &&
+                                                           e != EExternalDependency.JETTY_92_WEBAPP;
+
+    final Predicate <EExternalDependency> aIsBOM = e -> e == EExternalDependency.JAXB_BOM ||
+                                                        e == EExternalDependency.JERSEY2_BOM ||
+                                                        e == EExternalDependency.JAXWS_RI_BOM;
+
+    final IMicroElement eDeps = eProject.appendElement (NS, "dependencies");
+    for (final EExternalDependency e : EExternalDependency.values ())
+      if (aFilter.test (e))
+      {
+        final IMicroElement eDep = eDeps.appendElement (NS, "dependency");
+        eDep.appendElement (NS, "groupId").appendText (e.getGroupID ());
+        eDep.appendElement (NS, "artifactId").appendText (e.getArtifactID ());
+        eDep.appendElement (NS, "version").appendText (e.getLastPublishedVersionString ());
+        if (aIsBOM.test (e))
+        {
+          eDep.appendElement (NS, "type").appendText ("pom");
+        }
+      }
+
+    {
+      final IMicroElement eBuild = eProject.appendElement (NS, "build");
+      final IMicroElement ePlugins = eBuild.appendElement (NS, "plugins");
+      final IMicroElement ePlugin = ePlugins.appendElement (NS, "plugin");
+      ePlugin.appendElement (NS, "groupId").appendText ("org.codehaus.mojo");
+      ePlugin.appendElement (NS, "artifactId").appendText ("versions-maven-plugin");
+      ePlugin.appendElement (NS, "version").appendText ("2.2");
+      final IMicroElement eConfig = ePlugin.appendElement (NS, "configuration");
+      eConfig.appendElement (NS, "allowSnapshots").appendText ("false");
+    }
+
+    final File f = new File ("deps/pom.xml");
+    MicroWriter.writeToFile (aDoc, f);
+    s_aLogger.info ("Done");
+    s_aLogger.info ("Run the following now on " + f.toString () + ": mvn versions:display-dependency-updates");
+  }
+}
