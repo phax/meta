f6c28f7f3517d38183c20c0501b17183d5639f25
Philip Helger <philip@helger.com>
2016-09-09 11:44:39 +0200

    backtracking

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -34,48 +34,82 @@ public class Solution
     private final int m_entries;
     private final boolean [] m_free;
     private final int m_jump;
-    private final BitSet m_visited;
 
     public Board (final boolean [] free, final int m)
     {
       m_entries = free.length;
       m_free = free;
       m_jump = m;
-      m_visited = new BitSet (m_entries);
       assert m_free[0];
     }
 
-    private boolean _isFree (final int pos)
+    private boolean _isFree (final int pos, final BitSet visited)
     {
-      return pos >= 0 && (pos >= m_entries || (m_free[pos] && !m_visited.get (pos)));
+      return pos >= 0 && (pos >= m_entries || (m_free[pos] && !visited.get (pos)));
     }
 
-    private int _canJumpForward (final int startPos)
+    private boolean _isWin (final int pos)
     {
-      if (_isFree (startPos + m_jump))
-        return startPos + m_jump;
-      if (_isFree (startPos + 1))
-        return startPos + 1;
-      if (_isFree (startPos - 1))
-        return startPos - 1;
-      return -1;
+      return pos >= m_entries;
+    }
+
+    private boolean _canWin (final int startPos, final BitSet visited, final int level)
+    {
+      String prefix = "";
+      for (int i = 0; i < level; ++i)
+        prefix += "  ";
+
+      int nextPos = startPos + m_jump;
+      if (_isFree (nextPos, visited))
+      {
+        if (_isWin (nextPos))
+          return true;
+
+        if (false)
+          System.out.println (prefix + "Jumping from " + startPos + " to " + nextPos);
+        final BitSet visited2 = (BitSet) visited.clone ();
+        visited2.set (nextPos, true);
+        if (_canWin (nextPos, visited2, level + 1))
+          return true;
+        // else continue
+      }
+
+      nextPos = startPos + 1;
+      if (_isFree (nextPos, visited))
+      {
+        if (_isWin (nextPos))
+          return true;
+
+        if (false)
+          System.out.println (prefix + "Jumping from " + startPos + " to " + nextPos);
+        final BitSet visited2 = (BitSet) visited.clone ();
+        visited2.set (nextPos, true);
+        if (_canWin (nextPos, visited2, level + 1))
+          return true;
+        // else continue
+      }
+
+      nextPos = startPos - 1;
+      if (_isFree (nextPos, visited))
+      {
+        if (false)
+          System.out.println (prefix + "Jumping from " + startPos + " to " + nextPos);
+
+        final BitSet visited2 = (BitSet) visited.clone ();
+        visited2.set (nextPos, true);
+        if (_canWin (nextPos, visited2, level + 1))
+          return true;
+        // else continue
+      }
+      return false;
     }
 
     public boolean canWin ()
     {
-      int pos = 0;
-      while (true)
-      {
-        final int oldPos = pos;
-        pos = _canJumpForward (pos);
-        if (pos < 0)
-          return false;
-        if (false)
-          System.out.println ("Jumped from " + oldPos + " to " + pos);
-        m_visited.set (pos);
-        if (pos >= m_entries)
-          return true;
-      }
+      final BitSet visited = new BitSet (m_entries);
+      final int pos = 0;
+      visited.set (pos, true);
+      return _canWin (pos, visited, 0);
     }
   }
 }
