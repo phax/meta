e9bd30a5afa31ac8d0e0d7eb4834f8159292ee50
Philip Helger <philip@helger.com>
2018-01-05 19:13:10 +0100

    Version update

--- src/main/java/com/helger/meta/project/EExternalDependency.java
+++ src/main/java/com/helger/meta/project/EExternalDependency.java
@@ -44,7 +44,7 @@ public enum EExternalDependency
   BC_PROV_EXT ("org.bouncycastle", "bcprov-ext-jdk15on", BC_MAIL),
   BC_PKIX ("org.bouncycastle", "bcpkix-jdk15on", BC_MAIL),
 
-  CLOSURE ("com.google.javascript", "closure-compiler", "v20171203", EJDK.JDK8),
+  CLOSURE ("com.google.javascript", "closure-compiler", "v20180101", EJDK.JDK8),
   CODEMODEL ("com.sun.codemodel", "codemodel", "2.6", EJDK.JDK6),
   COMMONS_BEANUTILS ("commons-beanutils", "commons-beanutils", "1.9.3", EJDK.JDK6),
   COMMONS_COLLECTIONS_3 ("commons-collections", "commons-collections", "3.2.2", EJDK.JDK6),
