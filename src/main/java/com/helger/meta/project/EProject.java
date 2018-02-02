92f2858d5099e136eedbe429663a1110fbea30e7
Philip Helger <philip@helger.com>
2018-02-02 15:40:56 +0100

    Version update

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -414,7 +414,7 @@ public enum EProject implements IProject
                             EProjectType.MAVEN_POM,
                             EHasPages.TRUE,
                             EHasWiki.FALSE,
-                            "5.0.0",
+                            "5.0.1",
                             EJDK.JDK8),
   PH_SCHEMATRON (PH_SCHEMATRON_PARENT_POM, "ph-schematron", EProjectType.JAVA_LIBRARY),
   PH_SCHEMATRON_TESTFILES (PH_SCHEMATRON_PARENT_POM, "ph-schematron-testfiles", EProjectType.JAVA_LIBRARY),
