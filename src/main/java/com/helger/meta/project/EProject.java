3c31dc3474d1334b267b3ed682153a513b733b25
Philip Helger <philip@helger.com>
2018-11-23 16:24:18 +0100

    Version update

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -657,7 +657,7 @@ public enum EProject implements IProject
                            EProjectType.JAVA_LIBRARY,
                            EHasPages.FALSE,
                            EHasWiki.FALSE,
-                           "3.0.3",
+                           "3.0.4",
                            EJDK.JDK8),
   ERECHNUNG_WS_CLIENT (null,
                        IProject.DEFAULT_PROJECT_OWNER,
