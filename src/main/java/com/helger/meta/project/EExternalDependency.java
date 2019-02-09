b8db0a44c46b554c74c4c3e7c741949c24e0d07a
Philip Helger <philip@helger.com>
2019-02-09 21:03:02 +0100

    Verison update

--- src/main/java/com/helger/meta/project/EExternalDependency.java
+++ src/main/java/com/helger/meta/project/EExternalDependency.java
@@ -204,10 +204,8 @@ public enum EExternalDependency
   JUNIT5_JUPITER_API ("org.junit.jupiter", "junit-jupiter-api", "5.4.0", EJDK.JDK8),
   JUNIT5_JUPITER_ENGINE ("org.junit.jupiter", "junit-jupiter-engine", JUNIT5_JUPITER_API),
   JUNIT5_PLATFORM_LAUNCHER ("org.junit.platform", "junit-platform-launcher", "1.4.0", EJDK.JDK8),
-  JUNIT5_PLATFORM_SUREFIRE_PROVIDER ("org.junit.platform",
-                                     "junit-platform-surefire-provider",
-                                     JUNIT5_PLATFORM_LAUNCHER),
-  JUNIT5_VINTAGE_ENGINE ("org.junit.vintage", "junit-vintage-engine", JUNIT5_JUPITER_API),
+  JUNIT5_PLATFORM_SUREFIRE_PROVIDER ("org.junit.platform", "junit-platform-surefire-provider", "1.3.2", EJDK.JDK8),
+  JUNIT5_VINTAGE_ENGINE ("org.junit.vintage", "junit-vintage-engine", "5.3.2", EJDK.JDK8),
 
   KAFKA_CLIENT ("org.apache.kafka", "kafka-clients", "2.1.0", EJDK.JDK8),
   LITTLEPROXY ("org.littleshoot", "littleproxy", "1.1.2", EJDK.JDK8),
