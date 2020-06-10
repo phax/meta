aac120f1eeade58c1f709b61a7691e400da855e0
Philip Helger <philip@helger.com>
2020-06-11 00:37:27 +0200

    Version update

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -585,7 +585,7 @@ public enum EProject implements IProject
                             EProjectType.MAVEN_POM,
                             EHasPages.FALSE,
                             EHasWiki.FALSE,
-                            "1.0.0",
+                            "1.0.1",
                             EJDK.JDK8),
   PH_BDVE_RULES_CII (PH_BDVE_RULES_PARENT_POM, "ph-bdve-rules-cii", EProjectType.JAVA_LIBRARY),
   PH_BDVE_RULES_EBINTERFACE (PH_BDVE_RULES_PARENT_POM, "ph-bdve-rules-ebinterface", EProjectType.JAVA_LIBRARY),
