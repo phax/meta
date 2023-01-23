73b5a1cdaeea777c4ae3bbcab0fbd3c43ff10043
Philip Helger <philip@helger.com>
2023-01-23 20:34:35 +0100

    Updated

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -578,7 +578,7 @@ public enum EProject implements IProject
                              EProjectType.MAVEN_POM,
                              EHasPages.FALSE,
                              EHasWiki.FALSE,
-                             "8.8.3",
+                             "8.8.4",
                              EJDK.JDK8),
   PEPPOL_ID_DATATYPES (PEPPOL_COMMONS_PARENT_POM, "peppol-id-datatypes", EProjectType.JAVA_LIBRARY),
   PEPPOL_ID (PEPPOL_COMMONS_PARENT_POM, "peppol-id", EProjectType.JAVA_LIBRARY),
