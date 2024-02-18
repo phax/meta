588dd2ec2c3f3dfee21842f2c3211896c1031d1a
Philip Helger <philip@helger.com>
2024-02-18 20:05:37 +0100

    Updated

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -632,7 +632,7 @@ public enum EProject implements IProject
                           EProjectType.MAVEN_POM,
                           EHasPages.FALSE,
                           EHasWiki.FALSE,
-                          "3.1.6",
+                          "3.1.7",
                           EJDK.JDK11),
   PHIVE_RULES_CII (PHIVE_RULES_PARENT_POM, "phive-rules-cii", EProjectType.JAVA_LIBRARY),
   PHIVE_RULES_CIUS_PT (PHIVE_RULES_PARENT_POM, "phive-rules-cius-pt", EProjectType.JAVA_LIBRARY),
