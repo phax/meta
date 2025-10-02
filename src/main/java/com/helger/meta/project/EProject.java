c95ed85e1372c2e235350d38fd2820b67c0c4256
Philip Helger <philip@helger.com>
2025-10-02 22:51:23 +0200

    Updated

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -680,6 +680,7 @@ public enum EProject implements IProject
   PHIVE_RULES_FINVOICE (PHIVE_RULES_PARENT_POM, "phive-rules-finvoice", EProjectType.JAVA_LIBRARY),
   PHIVE_RULES_FRANCE (PHIVE_RULES_PARENT_POM, "phive-rules-france", EProjectType.JAVA_LIBRARY),
   PHIVE_RULES_ISDOC (PHIVE_RULES_PARENT_POM, "phive-rules-isdoc", EProjectType.JAVA_LIBRARY),
+  PHIVE_RULES_KSEF (PHIVE_RULES_PARENT_POM, "phive-rules-ksef", EProjectType.JAVA_LIBRARY),
   PHIVE_RULES_OIOUBL (PHIVE_RULES_PARENT_POM, "phive-rules-oioubl", EProjectType.JAVA_LIBRARY),
   PHIVE_RULES_PEPPOL (PHIVE_RULES_PARENT_POM, "phive-rules-peppol", EProjectType.JAVA_LIBRARY),
   PHIVE_RULES_PEPPOL_ITALY (PHIVE_RULES_PARENT_POM, "phive-rules-peppol-italy", EProjectType.JAVA_LIBRARY),
