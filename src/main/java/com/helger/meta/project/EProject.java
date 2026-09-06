f6f49cf2ffce6bbe3141eaf353274151d55cda60
Philip Helger <philip@helger.com>
2026-09-06 15:30:11 +0200

    Updated

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -531,7 +531,7 @@ public enum EProject implements IProject
                       EProjectType.MAVEN_POM,
                       EHasPages.FALSE,
                       EHasWiki.FALSE,
-                      "10.5.0",
+                      "10.6.0",
                       EJDK.JDK17),
   PH_OTON_HTML (PH_OTON_PARENT_POM, "ph-oton-html", EProjectType.JAVA_LIBRARY),
   PH_OTON_MARKDOWN (PH_OTON_PARENT_POM, "ph-oton-markdown", EProjectType.JAVA_LIBRARY),
