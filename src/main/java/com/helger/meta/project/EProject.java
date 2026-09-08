b4f529b3fd3ba0772b0090bfff1558c5abb126fc
Philip Helger <philip@helger.com>
2026-09-09 00:17:16 +0200

    Updated

--- src/main/java/com/helger/meta/project/EProject.java
+++ src/main/java/com/helger/meta/project/EProject.java
@@ -1056,7 +1056,7 @@ public enum EProject implements IProject
                               EProjectType.MAVEN_POM,
                               EHasPages.FALSE,
                               EHasWiki.FALSE,
-                              "0.18.0",
+                              "0.18.1",
                               EJDK.JDK17),
   PHOSS_DIRECTORY_INDEXER (PHOSS_DIRECTORY_PARENT_POM, "phoss-directory-indexer", EProjectType.JAVA_LIBRARY),
   PHOSS_DIRECTORY_INDEXER_CONFORMANCE (PHOSS_DIRECTORY_PARENT_POM,
